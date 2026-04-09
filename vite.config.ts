8a95dcf67a2a535c55296b90d9006eaa7e7984d1
DonutExtreme <muhddanish704@gmail.com>
2026-04-09 15:14:13 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/wedding-invitaions/" : "/",
+  base: mode === "production" ? "/wedding-invitations/" : "/",
   server: {
     host: "::",
     port: 8080,
